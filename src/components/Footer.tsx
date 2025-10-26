a5ef469e429dc24b3d9536ff381150f2490b33df
AnonXarkA <arkanath41@gmail.com>
2025-10-26 21:47:52 +0600

    -

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 export const Footer = () => {
   return (
     <footer className="w-full py-4 text-center text-neutral-400 text-sm bg-black bg-opacity-80 backdrop-blur-sm z-40">
-      <p>&copy; {new Date().getFullYear()} Arka Debnath. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Arka Debnath (AnonXarkA) . All rights reserved.</p>
     </footer>
   );
 };
